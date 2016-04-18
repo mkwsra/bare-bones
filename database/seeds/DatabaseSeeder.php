a24c7caac99abd23a249cffbf4dd9dffadab4c8c
Mohamed Kawsara <mkwsra@gmail.com>
2016-04-18 19:07:21 +0300

    Build a simple user system and add seeder to add dummy users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        \App\User::create([
+            'name' => 'Dummy User',
+            'email' => 'your@emailaddress.com',
+            'password' => bcrypt('LaravelTestPW'),
+        ]);
+        \App\User::create([
+            'name' => 'Mohamed Kawsara',
+            'email' => 'mkwsra@gmail.com',
+            'password' => bcrypt('LaravelTestPW'),
+        ]);
+        \App\User::create([
+            'name' => 'Jeremy Bilawa',
+            'email' => 'git@mcclainconcepts.com',
+            'password' => bcrypt('LaravelTestPW'),
+        ]);
     }
 }
